c12ac86dba8bb751731793fc6004dae55f5b4a34
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-09-08 18:24:53 +0500

    Task1

--- Tyuiu.MalkovaMS.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.MalkovaMS.Sprint0.Task1.V0/Program.cs
@@ -2,10 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World");
-        string name = "Игорь";
-        int age = 45;
-        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+        Console.WriteLine("Hello, World!");
+        string name = "Мария";
+        int age = 17;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
         Console.ReadKey();
     }
 }
